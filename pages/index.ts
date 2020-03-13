bfd1bd57d28bfd99edb08df461886a7240550b18
Aqrun <aqrun@sina.com>
2020-03-13 13:30:30 +0800

    init

--- pages/index.ts
+++ pages/index.ts
@@ -1,4 +1,1 @@
 export { default as Agent } from './agent'
-export { default as Dashboard } from './dashboard'
-export { default as Htlp } from './htlp'
-export { default as MyCruise } from './my-cruise'
