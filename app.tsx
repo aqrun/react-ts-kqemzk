7e91bf50725028d35d06548eeeacd66bfbdd42e4
Aqrun <aqrun@sina.com>
2020-03-13 12:56:25 +0800

    init

---
+++ app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+    Header  
+} from './components'
+
+export function App(){
+    return (
+        <div>
+            <p>app page</p>
+            <Header />
+        </div>
+    )
+}
--- index.tsx
+++ index.tsx
@@ -1,31 +1,7 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './app'
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    ( <App />), document.getElementById('root'));
